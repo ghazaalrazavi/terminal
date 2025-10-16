ef674257e78d3cf5684b11b171b01cf4b0e25074
ghazaal razavi <ghazraz221b@gmail.com>
2025-10-16 21:10:25 +0330

    classes has been completed

--- classes.py
+++ classes.py
@@ -1,24 +1,72 @@
 from validate import *
+from pprint import pprint
+
+class User:
+    def __init__(self, email, username, password):
+        self.email = validate_email(email)
+        self.username = validate_username(username)
+        self.password = validate_password(password)
 
 
 class Trip:
-    def __init__(self,cost,start_time,end_time,status = "pending"):
-        self.cost = cost
-        self.start_time = start_time
-        self.end_time = end_time
+    def __init__(self, start, end, status="pending"):
+        self.start = start
+        self.end = end
         self.status = status
 
-class User:
-    def __init__(self,username,password,user_type = "default_user"):
-        self.username = validate_username(username)
-        self.password = validate_password(password)
-        self.user_type = user_type
+    def change_status(self, status):
+        if status not in ("canceled", "done", "pending"):
+            raise ValueError("you can only choose between pending,done and canceled")
+        self.status = status
 
-class Dashboard:
-    def __init__(self,user:User,wallet):
+    def __str__(self):
+        return f"{self.start} => {self.end} | Status: {self.status}"
+
+
+class Ticket:
+    def __init__(self, trip: Trip, cost):
+        self.trip = trip
+        self.cost = cost
+
+    def __str__(self):
+        return f"trip: {self.trip} | Cost: {self.cost}$"
+
+
+class Passenger(User):
+    def __init__(self, email, username, password):
+        super().__init__(email, username, password)
+        self.tickets = []
+
+    def get_ticket(self, ticket: Ticket):
+        if isinstance(ticket, Ticket):
+            self.tickets.append(ticket)
+        else:
+            raise TypeError("ticket type is not matched")
+
+
+class SuperUser(User):
+    def __init__(self, email, username, password):
+        super().__init__(email, username, password)
+
+class Dashboard():
+    def __init__(self,user:Passenger,wallet):
         self.user = user
         self.__wallet = wallet
 
-    @property
+    @property 
     def wallet(self):
         return self.__wallet
+    
+    def charge_wallet(self,amount):
+        try:
+            if amount < 0:
+                raise NegativeValue("cant be smaller than 0 sir")
+            self.__wallet+=amount
+            print(f"the amount:{amount}$ just added to ur wallet new balance: {self.__wallet} ")
+        except NegativeValue as e:
+            print(f'error: {e}')
+        
+    def history(self,user:Passenger):
+        for trip in user.tickets:
+            pprint(trip)
+    
